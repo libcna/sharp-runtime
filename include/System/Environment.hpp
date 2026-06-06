b1ef694e865da43e0ad1a65f92a0d3c41bf40a6f
Robert Vokac <robertvokac@robertvokac.com>
2026-06-06 18:09:33 +0200

    Add Environment::SpecialFolder and SpecialFolderOption enums

--- include/System/Environment.hpp
+++ include/System/Environment.hpp
@@ -20,6 +20,62 @@ namespace System {
     public:
         Environment() = delete;
 
+        enum class SpecialFolder {
+            Desktop                  = 0x0000,
+            Programs                 = 0x0002,
+            Personal                 = 0x0005,
+            MyDocuments              = 0x0005,
+            Favorites                = 0x0006,
+            Startup                  = 0x0007,
+            Recent                   = 0x0008,
+            SendTo                   = 0x0009,
+            StartMenu                = 0x000B,
+            MyMusic                  = 0x000D,
+            MyVideos                 = 0x000E,
+            DesktopDirectory         = 0x0010,
+            MyComputer               = 0x0011,
+            NetworkShortcuts         = 0x0013,
+            Fonts                    = 0x0014,
+            Templates                = 0x0015,
+            CommonStartMenu          = 0x0016,
+            CommonPrograms           = 0x0017,
+            CommonStartup            = 0x0018,
+            CommonDesktopDirectory   = 0x0019,
+            ApplicationData          = 0x001A,
+            PrinterShortcuts         = 0x001B,
+            LocalApplicationData     = 0x001C,
+            InternetCache            = 0x0020,
+            Cookies                  = 0x0021,
+            History                  = 0x0022,
+            CommonApplicationData    = 0x0023,
+            Windows                  = 0x0024,
+            System                   = 0x0025,
+            ProgramFiles             = 0x0026,
+            MyPictures               = 0x0027,
+            UserProfile              = 0x0028,
+            SystemX86                = 0x0029,
+            ProgramFilesX86          = 0x002A,
+            CommonProgramFiles       = 0x002B,
+            CommonProgramFilesX86    = 0x002C,
+            CommonTemplates          = 0x002D,
+            CommonDocuments          = 0x002E,
+            CommonAdminTools         = 0x002F,
+            AdminTools               = 0x0030,
+            CommonMusic              = 0x0035,
+            CommonPictures           = 0x0036,
+            CommonVideos             = 0x0037,
+            Resources                = 0x0038,
+            LocalizedResources       = 0x0039,
+            CommonOemLinks           = 0x003A,
+            CDBurning                = 0x003B,
+        };
+
+        enum class SpecialFolderOption {
+            None        = 0,
+            DoNotVerify = 0x4000,
+            Create      = 0x8000,
+        };
+
 #ifdef _WIN32
         static inline const std::string NewLine = "\r\n";
 #else
