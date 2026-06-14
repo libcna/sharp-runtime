a80e6ad104cb531345d40148ca1f46e6011d0888
Robert Vokac <robertvokac@robertvokac.com>
2026-06-14 16:51:02 +0200

    TypeUnloadedException: use exception_ptr for inner; Doxygen comments

--- include/System/TypeUnloadedException.hpp
+++ include/System/TypeUnloadedException.hpp
@@ -6,12 +6,19 @@
 
 namespace System {
 
+    /**
+     * @brief The exception that is thrown when there is an attempt to access a
+     * type that has been unloaded.
+     */
     class TypeUnloadedException : public SystemException {
     public:
+        /** @brief Initializes a new instance with the default message. */
         TypeUnloadedException() : SystemException("Type had been unloaded.") {}
+        /** @brief Initializes a new instance with the specified error message. */
         explicit TypeUnloadedException(const std::string& message) : SystemException(message) {}
-        TypeUnloadedException(const std::string& message, const std::exception& inner)
-            : SystemException(message + " | inner: " + inner.what()) {}
+        /** @brief Initializes a new instance with the specified message and inner exception. */
+        TypeUnloadedException(const std::string& message, std::exception_ptr inner)
+            : SystemException(message, inner) {}
     };
 
 } // namespace System
