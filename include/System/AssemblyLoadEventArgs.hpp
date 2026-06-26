6a8c3db6e435e50499198e5d82702bc8e9b7dd3f
Robert Vokac <robertvokac@robertvokac.com>
2026-06-26 15:32:35 +0200

    Port AssemblyLoadEventArgs + AssemblyLoadEventHandler: upgrade to Doxygen comments, add 4 tests

--- include/System/AssemblyLoadEventArgs.hpp
+++ include/System/AssemblyLoadEventArgs.hpp
@@ -2,24 +2,45 @@
 // Copyright (c) Robert Vokac and contributors
 // Portions based on .NET runtime API (MIT License, Copyright .NET Foundation and Contributors)
 #pragma once
+#include <functional>
 #include <string>
 #include "System/EventArgs.hpp"
 
 namespace System {
 
-    /// Provides data for the event that is raised when an assembly is loaded.
-    class AssemblyLoadEventArgs : public EventArgs {
-        std::string assemblyName_;
-    public:
-        /// Initializes a new instance with the name of the loaded assembly.
-        explicit AssemblyLoadEventArgs(const std::string& assemblyName)
-            : assemblyName_(assemblyName) {}
+/**
+ * @brief Provides data for the event that is raised when an assembly is loaded.
+ *
+ * C++ counterpart of .NET System.AssemblyLoadEventArgs.
+ * Since sharp-runtime has no reflection, the loaded assembly is represented
+ * by its name string rather than a System.Reflection.Assembly instance.
+ */
+class AssemblyLoadEventArgs : public EventArgs {
+    std::string assemblyName_;
+public:
+    /**
+     * @brief Initializes a new instance with the name of the loaded assembly.
+     *
+     * C++ counterpart of .NET AssemblyLoadEventArgs(Assembly loadedAssembly).
+     * @param assemblyName The name of the assembly that was loaded.
+     */
+    explicit AssemblyLoadEventArgs(const std::string& assemblyName)
+        : assemblyName_(assemblyName) {}
 
-        /// Returns the name of the assembly that was loaded.
-        [[nodiscard]] const std::string& getLoadedAssemblyProperty() const { return assemblyName_; }
-    };
+    /**
+     * @brief Gets the name of the assembly that was loaded.
+     *
+     * C++ counterpart of .NET AssemblyLoadEventArgs.LoadedAssembly.
+     * @return The assembly name string.
+     */
+    [[nodiscard]] const std::string& getLoadedAssemblyProperty() const { return assemblyName_; }
+};
 
-    /// Delegate type for assembly-load event handlers.
-    using AssemblyLoadEventHandler = std::function<void(void*, AssemblyLoadEventArgs&)>;
+/**
+ * @brief Delegate type for assembly-load event handlers.
+ *
+ * C++ counterpart of .NET System.AssemblyLoadEventHandler.
+ */
+using AssemblyLoadEventHandler = std::function<void(void*, AssemblyLoadEventArgs&)>;
 
 } // namespace System
