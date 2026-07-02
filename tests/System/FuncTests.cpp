90c0209e9d6ae331fce8988a896475c004de53a8
Robert Vokac <robertvokac@robertvokac.com>
2026-07-02 17:15:36 +0200

    Port Func: extend to FuncT4..FuncT16, matching .NET's Func<T1..T16,TResult> arities and reaching parity with the existing Action/ActionT..ActionT16 family; add regression tests for the new arities

--- tests/System/FuncTests.cpp
+++ tests/System/FuncTests.cpp
@@ -25,3 +25,25 @@ TEST(FuncTests, FuncT3_ThreeArgs_ReturnsConcatenated) {
         [](std::string a, std::string b, std::string c) { return a + b + c; };
     EXPECT_EQ(f("x", "y", "z"), "xyz");
 }
+
+TEST(FuncTests, FuncT4_FourArgs_ReturnsSum) {
+    System::FuncT4<int, int, int, int, int> f = [](int a, int b, int c, int d) { return a + b + c + d; };
+    EXPECT_EQ(f(1, 2, 3, 4), 10);
+}
+
+TEST(FuncTests, FuncT8_EightArgs_ReturnsSum) {
+    System::FuncT8<int, int, int, int, int, int, int, int, int> f =
+        [](int a, int b, int c, int d, int e, int g, int h, int i) {
+            return a + b + c + d + e + g + h + i;
+        };
+    EXPECT_EQ(f(1, 2, 3, 4, 5, 6, 7, 8), 36);
+}
+
+TEST(FuncTests, FuncT16_SixteenArgs_ReturnsSum) {
+    System::FuncT16<int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int> f =
+        [](int a, int b, int c, int d, int e, int g, int h, int i, int j, int k, int l, int m,
+           int n, int o, int p, int q) {
+            return a + b + c + d + e + g + h + i + j + k + l + m + n + o + p + q;
+        };
+    EXPECT_EQ(f(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16), 136);
+}
