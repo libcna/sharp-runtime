10813161a85b6a27534e4ad334a8533451bbddad
Robert Vokac <robertvokac@robertvokac.com>
2026-07-13 14:41:37 +0200

    style(Win32Exception): convert public int params/field to intcs
    ticket 160, plan.sqlite3

--- include/System/ComponentModel/Win32Exception.hpp
+++ include/System/ComponentModel/Win32Exception.hpp
@@ -3,19 +3,22 @@
 // Portions based on .NET runtime API (MIT License, Copyright .NET Foundation and Contributors)
 #pragma once
 #include <string>
+#include "SharpRuntime/SharpRuntimeHelper.hpp"
 #include "System/Exception.hpp"
 
 namespace System::ComponentModel {
 
+    using SharpRuntime::intcs;
+
     /** Represents an exception thrown by a Win32 API call, carrying the native error code. */
     class Win32Exception : public System::Exception {
-        int nativeErrorCode_;
+        intcs nativeErrorCode_;
     public:
         /**
          * Constructs a Win32Exception from a numeric Windows error code.
          * @param errorCode The Win32 error code (e.g. from GetLastError()).
          */
-        explicit Win32Exception(int errorCode)
+        explicit Win32Exception(intcs errorCode)
             : System::Exception("Win32 error " + std::to_string(errorCode)),
               nativeErrorCode_(errorCode) {}
 
@@ -24,11 +27,11 @@ namespace System::ComponentModel {
          * @param errorCode The Win32 error code.
          * @param message   A human-readable description of the error.
          */
-        Win32Exception(int errorCode, const std::string& message)
+        Win32Exception(intcs errorCode, const std::string& message)
             : System::Exception(message), nativeErrorCode_(errorCode) {}
 
         /** @return The underlying Win32 error code. */
-        [[nodiscard]] int getNativeErrorCodeProperty() const { return nativeErrorCode_; }
+        [[nodiscard]] intcs getNativeErrorCodeProperty() const { return nativeErrorCode_; }
     };
 
 } // namespace System::ComponentModel
