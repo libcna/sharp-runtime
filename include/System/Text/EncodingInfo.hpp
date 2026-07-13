e6c17be7a4b0ea712a0510ef549bc96648505738
Robert Vokac <robertvokac@robertvokac.com>
2026-07-13 15:02:43 +0200

    style(EncodingInfo): convert public int API surface to SharpRuntime::intcs
    ticket 229, plan.sqlite3

--- include/System/Text/EncodingInfo.hpp
+++ include/System/Text/EncodingInfo.hpp
@@ -4,23 +4,24 @@
 #pragma once
 #include <memory>
 #include <string>
+#include "SharpRuntime/SharpRuntimeHelper.hpp"
 #include "System/Text/Encoding.hpp"
 
 namespace System::Text {
 
     /** Provides information about a character encoding (code page, name, display name). */
     class EncodingInfo {
-        int codePage_;
+        SharpRuntime::intcs codePage_;
         std::string name_;
         std::string displayName_;
 
     public:
         /** Constructs an EncodingInfo with the given code-page identifier, IANA name, and display name. */
-        EncodingInfo(int codePage, std::string name, std::string displayName)
+        EncodingInfo(SharpRuntime::intcs codePage, std::string name, std::string displayName)
             : codePage_(codePage), name_(std::move(name)), displayName_(std::move(displayName)) {}
 
         /** Gets the code-page identifier for this encoding. */
-        [[nodiscard]] int getCodePageProperty() const { return codePage_; }
+        [[nodiscard]] SharpRuntime::intcs getCodePageProperty() const { return codePage_; }
         /** Gets the IANA name of this encoding. */
         [[nodiscard]] const std::string& getNameProperty() const { return name_; }
         /** Gets the human-readable name of this encoding. */
